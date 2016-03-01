ac8d31db597e4cf01e11851c24f8852a7f637028
Evandson Maranhão <evandson.cm@gmail.com>
2016-02-29 21:45:45 -0300

    Final version matkids

--- matkids/js/myjs.js
+++ matkids/js/myjs.js
@@ -25,5 +25,23 @@ function multiplicar(n1, n2){
         var mult = parseInt(n1) * parseInt(n2);
         document.getElementById('mult').value = mult
     }
+function media(n1, n2){
+        var n1 = document.getElementById("n1").value;
+        var n2 = document.getElementById("n2").value;
+        var mult = parseInt(n1) + parseInt(n2);
+        var med = mult / 2;
+        document.getElementById('med').value = med
+    }
 
+function maior(n1, n2){
+        var n1 = document.getElementById("n1").value;
+        var n2 = document.getElementById("n2").value;
+        var mai = Math.max(n1,n2);
+        document.getElementById('mai').value = mai
+    }
 
+function raizQuadrada(n1){
+        var n1 = document.getElementById("n1").value;
+        var raiz = Math.sqrt(n1);
+        document.getElementById('raiz').value = raiz
+    }
